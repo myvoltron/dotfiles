18449286d71bd09f8b59efe95bbab60243589b71
satan <leejs8266@gmail.com>
2026-05-01 01:59:57 +0900

    feat: 선택한 라인의 번호를 클립보드에 저장하는 함수 추가

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("v", "<leader>yl", function()
+  local start_line = vim.fn.line("v")
+  local end_line = vim.fn.line(".")
+
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+
+  local file = vim.fn.expand("%")
+  local text = string.format("@%s\nL%d-L%d", file, start_line, end_line)
+
+  vim.fn.setreg("+", text)
+
+  vim.notify("Copied file range: " .. text)
+end, { desc = "Copy file range" })
